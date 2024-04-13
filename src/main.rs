eba0112740575fdd0c625c5316d3aab32cc1e36e
s0uthview <saoirse@foureyeddeer.com>
2024-04-13 06:46:23 -0400

    cleanup again...

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,57 @@
 use std::time::Duration;
-
 use sysinfo::System;
 use discord_rpc_client::Client;
-use tokio::{
-    task,
-    time,
-};
+use tokio::{task, time};
 
-mod winapi;
-
-const CLIENT_KEY: u64 = 1228513276720058388; // replace this value with the client key for your discord application
+const CLIENT_KEY: u64 = 1228513276720058388; // replace this value with the client key for your Discord application
+const EXECUTABLE_NAME: &str = "FL64.exe";
 
 #[tokio::main]
 async fn main() {
-    // create a new discord RPC client
+    // Create a new Discord RPC client
     let mut drpc = Client::new(CLIENT_KEY);
 
     drpc.on_ready(|_ctx| {
         println!("FL Studio rich presence loaded with client ID {}", CLIENT_KEY);
     });
 
-    drpc.start(); // start the rpc client
+    // start the RPC client
+    drpc.start();
 
     let s = System::new_all();
 
-    // todo: add the image for FL studio to the rich presence
-    // todo: add external interaction with winapi to view window titles
-    
+    // start the task to update presence
     let update_process = task::spawn(async move {
         let mut interval = time::interval(Duration::from_secs(1));
 
         loop {
             interval.tick().await;
-        
-            let process_name: &str = "FL Studio";
 
-            // find instances of FL studio
-            let process = s.processes_by_name("FL64")
+            // Find the process by name and check to see if FL Studio is running
+            let _process = s.processes_by_name(EXECUTABLE_NAME)
                 .next()
-                .expect("No DAW processes found.");
-            let runtime: u64 = process.run_time();
+                .unwrap_or_else(|| {
+                    eprintln!("No DAW processes found.");
+                    std::process::exit(1);
+                });
 
+            let process_name = "FL Studio";
+
+            // let runtime: u64 = process.run_time();
+
+            // Set activity
             if let Err(msg) = drpc.set_activity(|a| a
-                        .state(process_name)
-                        .assets(|assets| assets.large_text(runtime.to_string()))) {
-                println!("! Failed to set presence: {}", msg)
+                .state(process_name)
+                .assets(|asset| asset.large_image("fl-studio"))
+            )
+            {
+                eprintln!("Failed to set presence: {}", msg)
             }
         }
     });
 
-    let _ = update_process.await;
+    // Await the completion of the update process task
+    if let Err(err) = update_process.await {
+        eprintln!("Failed to await update process task: {}", err);
+    }
 }
